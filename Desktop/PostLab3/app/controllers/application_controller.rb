387aceed06a9dbef66a2193293efaf819694fa8b
joseyvimas <jyvimas@gmail.com>
2017-04-08 15:29:53 -0400

    Agregar password y confirmacion de password
    Agregar password y confirmacion de password como parametros recibidos

--- Desktop/PostLab3/app/controllers/application_controller.rb
+++ Desktop/PostLab3/app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 	protected
 
         def configure_permitted_parameters
-            devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:nombre, :email, :encrypted_password, :password, :username, :nacimiento, :genero) }
+            devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:nombre, :email, :password, :password_confirmation, :username, :nacimiento, :genero) }
             
         end
 end
