a06fa33c8e53489fc614f9250e081236126688fe
joseyvimas <jyvimas@gmail.com>
2017-04-08 15:25:01 -0400

    Agregar validaciones de contraseña, genero y fecha

--- Desktop/PostLab3/app/models/user.rb
+++ Desktop/PostLab3/app/models/user.rb
@@ -4,11 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   
-  validates :email, presence: true, uniqueness: true
-  validates :username, presence: true, uniqueness: true
-  validates :encrypted_password, presence: true
-  validates :nombre, presence: true, length: { minimum: 2 }
-  validates :nacimiento, presence: true
-  validates :genero, presence: true, length: { is: 1 }
+  validates :email, :presence => {:message => ": el email no debe estar vacío."}, uniqueness: true
+  validates :username, :presence => {:message => ": el nombre de usuario no debe estar vacío."}, uniqueness: true
+  validates :password, :presence => {:message => ": la contraseña no debe estar vacía."}
+  validates :password_confirmation, :presence => {:message => ": la confirmación de su contraseña no debe estar vacía."}
+  validates :nombre, :presence => {:message => ": no debe estar vacío."}, length: { minimum: 2, :message => ": debe contener al menos dos caracteres."}
+  validates :nacimiento, :presence => {:message => ": no debe estar vacía."}
+  validates :genero, :presence => {:message => ": no debe estar vacío."}, length: { is: 1, :message => ": debe contener un solo caracter."}
+  validates_format_of :genero, :with => /(F|M)|(f|m)/, :message => ": debe ingresar un género valido (M o F)"
   
 end
